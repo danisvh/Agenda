d110bbff1464348a28166f23e14dc90381eea002
danisvh <danisvh@gmail.com>
2025-05-14 17:01:30 +0000

    Afegir funcionalitat per mostrar contactes

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // script.js
 console.log("Agenda Web");
+
+const contactes = ["Jose","Saida","Ruben","Vero","Jenny"];
+const llista = document.createElement('ul');
+contactes.forEach(nom => {
+    const li = document.createElement('li');
+    li.textContent = nom;
+    lista.appendCHiled(li);
+});
+document.body.appendChild(llista);
